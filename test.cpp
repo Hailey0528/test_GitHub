da39e1da56c207bb8926440045d0207b26540bad
Huanhuan Xiong <huanhuanxiong@Huanhuans-MacBook-Pro.local>
2018-05-08 10:32:51 +0800

    New test.cpp

---
+++ test.cpp
@@ -0,0 +1,23 @@
+# include <iostream >
+using namespace std ;
+
+int main ( int argc , char ** argv )
+{
+// definition the length of the array
+const int MAX_LEN = 5;
+
+// definition of the array
+int numbers [ MAX_LEN ] = {1 ,2 ,3 ,4 ,5};
+
+// calculation of the average
+float average = 0;
+for (int i = 0; i < MAX_LEN ; i++)
+{
+average += numbers [i]/( float ) MAX_LEN ;
+}
+
+// print the average
+cout << "The average is : " << average ;
+
+return 0;
+}
